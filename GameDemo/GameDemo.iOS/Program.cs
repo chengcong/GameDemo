71b1b3333ef7a1a65259a815c533a66150acceb3
chengcong <16750571@qq.com>
2017-11-16 10:39:37 +0800

    第一次提交项目

---
+++ GameDemo/GameDemo.iOS/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace GameDemo.iOS
+{
+    [Register("AppDelegate")]
+    class Program : UIApplicationDelegate
+    {
+        private static Game1 game;
+
+        internal static void RunGame()
+        {
+            game = new Game1();
+            game.Run();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+
+        public override void FinishedLaunching(UIApplication app)
+        {
+            RunGame();
+            AppCenter.Start("db6a324e-027e-4386-8e03-1a3ea250c27c",
+                   typeof(Analytics), typeof(Crashes));
+        }
+    }
+}
